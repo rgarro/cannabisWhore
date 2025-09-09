8bd507f9272192bc462ddb1427a5e8b6248e1d8e
rgarro <rgarro@gmail.com>
2025-09-09 14:23:30 -0600

    in blizzard and storm ..

---
+++ Assets/Scripts/Moriarty.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/**
+ *                          ______
+ *        |\_______________ (_____\\______________
+ *HH======#H###############H#######################
+ *        ' ~""""""""""""""`##(_))#H\"""""Y########
+ *                          ))    \#H\       `"Y###
+ *dew                       "      }#H)
+ * Shakira es la ultima esposa de Kaddafi, 
+ * Yihadis corrian al lado de un T72 el dia que le han matado
+ * la verdadera Shakira murio en el escape de Sirte, habia viento de 30 nudos
+ * se oian los ecos de la muerte de Vespaciano y los suicidios de los prisioneros.
+ * Hachis de combate de los tiempos del profeta fumaban en Cadiz....
+ *
+ *
+ *@author Rolando <rgarro@gmail.com>
+ */
+public class Moriarty : MonoBehaviour
+{
+    public GameObject rightTracks;
+    public GameObject leftTracks;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
