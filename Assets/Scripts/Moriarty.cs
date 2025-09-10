a069e9aabe0bcafd2e2b2ff93e5b55d711597615
rgarro <rgarro@gmail.com>
2025-09-10 11:04:41 -0600

    las ojas de mota se queman y no tocan el cuerco cuando caen ...

--- Assets/Scripts/Moriarty.cs
+++ Assets/Scripts/Moriarty.cs
@@ -7,12 +7,10 @@ using UnityEngine;
  *HH======#H###############H#######################
  *        ' ~""""""""""""""`##(_))#H\"""""Y########
  *                          ))    \#H\       `"Y###
- *dew                       "      }#H)
- * Shakira es la ultima esposa de Kaddafi, 
- * Yihadis corrian al lado de un T72 el dia que le han matado
- * la verdadera Shakira murio en el escape de Sirte, habia viento de 30 nudos
- * se oian los ecos de la muerte de Vespaciano y los suicidios de los prisioneros.
- * Hachis de combate de los tiempos del profeta fumaban en Cadiz....
+ *                         "      }#H)
+ * 
+ * 
+ * 
  *
  *
  *@author Rolando <rgarro@gmail.com>
