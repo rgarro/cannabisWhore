72270dd9fdf0266e92b25969cc087e776e2e595c
rgarro <rgarro@gmail.com>
2025-09-16 11:00:05 -0600

    seguro mataron a algun playo que fue de soplas a ver la guerra de ukrania y por eso ladran los perros ..

--- Assets/Scripts/turretController.cs
+++ Assets/Scripts/turretController.cs
@@ -1,9 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+/**
+* '\
+*        _\______
+*       /        \========
+*  ____|__________\_____
+* / ___________________ \
+* \/ _===============_ \/
+*   "-===============-"
+*
+*
+*@author Rolando <rgarro@gmail.com>
+*/
 public class turretController : MonoBehaviour
 {
+
+    public GameObject oddBallSpot;
+    
     // Start is called before the first frame update
     void Start()
     {
