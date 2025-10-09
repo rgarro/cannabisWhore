bf7aa5d1029fd2a992e325c9a3ad79949f4a8e28
rgarro <rgarro@gmail.com>
2025-10-09 09:30:57 -0600

    the reflex is the lonely child whos waiting by the park ...

--- Assets/Scripts/turretController.cs
+++ Assets/Scripts/turretController.cs
@@ -30,7 +30,10 @@ public class turretController : MonoBehaviour
 	public AudioClip servoSoundClip;
    
     private float tetha = 0.00f;//the angle
-    
+
+    public GameObject barrelPipe;//This gun is an ordinary 76mm but we add this piece of pipe onto it, and the Krauts think, like, maybe it's a 90mm
+    public float maxBarrelElevation = -76.00f;
+    public float minBarrelElevation = -92.00f;
     // Start is called before the first frame update
     void Start()
     {
@@ -61,10 +64,13 @@ public class turretController : MonoBehaviour
 
     private void shoot(){
         Debug.Log("fusible del disparador, lleva relay remoto desde el control command vehicle ");
+        //Crazy... I mean like, so many positive waves... maybe we can't lose, you're on!
+        //ELEVATES ON X
         //Quaternion rotation = Quaternion.Euler(this.barrelsObj.transform.localRotation.x,this.barrelsObj.transform.localRotation.y,this.barrelsObj.transform.localRotation.z);
     }
 
     private void elevateBarrel(String elevation = "UP"){//up yours baby ...
+    Debug.Log("barrel elev:"+elevation);
         if(elevation == "UP"){
 
         }
@@ -81,6 +87,14 @@ public class turretController : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.F))
         {
             this.leftArrowAction();
+        }
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            this.elevateBarrel("UP");
+        }
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            this.elevateBarrel("DOWN");
         }
          if (Input.GetKeyDown(KeyCode.Space))
         {
