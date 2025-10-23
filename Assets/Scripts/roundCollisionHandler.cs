9c3193ac153487bb30bf2f1336d67ff92708324f
rgarro <rgarro@gmail.com>
2025-10-23 17:44:52 -0600

    project update

---
+++ Assets/Scripts/roundCollisionHandler.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/**
+ *     _.-^^---....,,--       
+ * _--                  --_  
+ *<                        >)
+ *|                         | 
+ * \._                   _./  
+ *    ```--. . , ; .--'''       
+ *          | |   |             
+ *       .-=||  | |=-.   
+ *       `-=#$%&%$#=-'   
+ *          | ;  :|     
+ * _____.,-#%&$@%#&#~,._____
+ *
+ * === Will remove collided and trigger blast or visual effect ===
+ *   Includes Score Counter
+ *
+ * @author Rolando <rgarro@gmail.com>
+ */
+public class roundCollisionHandler : MonoBehaviour
+{
+    // Start is called before the first frame update
+    /*void Start()
+    {
+        
+    }*/
+    
+    void OnCollisionEnter(Collision collision){
+        Debug.Log("Collision detected with: " + collision.gameObject.name);
+    }
+
+    void OnCollisionStay(Collision collision)
+    {
+        Debug.Log("Still colliding with: " + collision.gameObject.name);
+    }
+
+    void OnCollisionExit(Collision collision)
+    {
+        Debug.Log("Stopped colliding with: " + collision.gameObject.name);
+    }
+
+    // Update is called once per frame
+    /*void Update()
+    {
+        
+    }*/
+}
