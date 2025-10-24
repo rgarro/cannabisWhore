a67f0c77ab4af5d954f54bbfbc6d55fda51a6409
rgarro <rgarro@gmail.com>
2025-10-24 09:56:13 -0600

    quedo guardado en la wolferda de jaco ..

--- Assets/Scripts/roundCollisionHandler.cs
+++ Assets/Scripts/roundCollisionHandler.cs
@@ -21,16 +21,79 @@ using UnityEngine;
  */
 public class roundCollisionHandler : MonoBehaviour
 {
+    public GameObject explosion;
+    public GameObject roundHit;
+
+    public string originTag = "m1tank";
+    public string scoreManagerTag = "BatComputer";
+    private GameObject scoreUpdater;
+    private GameObject damageCountdown;
+    public int ptsToIncrease = 10;
+    public bool isDamage = false;
+    
     // Start is called before the first frame update
-    /*void Start()
+    void Start()
     {
+        this.getScoreManager();
+    }
+
+    void getScoreManager(){
+        if(this.isDamage){
+            this.damageCountdown = GameObject.FindWithTag(this.scoreManagerTag);
+        }else{
+            this.scoreUpdater = GameObject.FindWithTag(this.scoreManagerTag);
+        }
         
-    }*/
+    }
+
+    public void increaseScore(){
+         if(this.isDamage){
+             damageCountdown tmpObj = this.damageCountdown.GetComponent(typeof(damageCountdown)) as damageCountdown;
+             //Debug.Log("decrease pts ...");
+            tmpObj.decreaseLife();
+        }else{
+            scoreDisplay tmpObj = this.scoreUpdater.GetComponent(typeof(scoreDisplay)) as scoreDisplay;
+            tmpObj.addScore(this.ptsToIncrease);
+        }
+    }
     
     private void OnTriggerEnter(Collider other)
     {
          Debug.Log("Collision detected+++  ");
          Debug.Log("Collision detected  "+other.gameObject.name);
+         //Debug.Log("collision .."); 
+        if(this.originTag == other.gameObject.tag){
+        
+        }else{
+             if(this.isDamage){
+           // Debug.Log("Checking Damage .."+this.scoreManagerTag +".."+other.gameObject.tag);
+            if (other.gameObject.CompareTag(this.scoreManagerTag)){
+           // Debug.Log("The Other .."+this.scoreManagerTag);    
+                this.increaseScore();
+            damageCountdown tmpObj = this.damageCountdown.GetComponent(typeof(damageCountdown)) as damageCountdown;
+            if(tmpObj.remainingLife < 0){
+                 GameObject e = Instantiate(this.explosion) as GameObject;
+                e.transform.position = transform.position;
+                Destroy(other.gameObject);
+                Destroy(this.gameObject);//fucking destroy
+                //POPUP RESTART HERE
+                    //post username and points to firebase
+                    //get top ten scores
+                    //legend pointing to restart
+            }else{
+                GameObject e = Instantiate(this.roundHit) as GameObject;
+                e.transform.position = other.gameObject.transform.position;
+                Destroy(this.gameObject);//fucking destroy
+            }
+            }
+        }else{
+             GameObject e = Instantiate(this.explosion) as GameObject;
+            e.transform.position = transform.position;
+            Destroy(other.gameObject);
+            Destroy(this.gameObject);//fucking destroy
+            this.increaseScore();
+        }
+        } 
     }
     
 
